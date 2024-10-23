8d29dec0507787d5bbd43d5f3c0b8548d1d070d0
Jorek57 <43407933+Jorek57@users.noreply.github.com>
2024-10-23 21:31:15 +0300

    Refac/update symfony (#35)
    update to Symfony 7.1

--- src/Security/AppCustomAuthentificatorAuthenticator.php
+++ src/Security/AppCustomAuthentificatorAuthenticator.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -29,7 +29,8 @@ class AppCustomAuthentificatorAuthenticator extends AbstractLoginFormAuthenticat
     {
         $email = $request->request->get('email', '');
 
-        $request->getSession()->set(Security::LAST_USERNAME, $email);
+        // Manually set the last username in the session
+        $request->getSession()->set('_security.last_username', $email);
 
         return new Passport(
             new UserBadge($email),
